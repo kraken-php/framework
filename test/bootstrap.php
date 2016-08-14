685744839a7a6b4a5ada68974da806b40787f6b1
Khelle <jamroz.kamil@gmail.com>
2016-08-14 16:42:59 +0200

    KRF-212 #resolve Added Throwable unit tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -7,4 +7,10 @@ if (!defined('PHPUNIT_COMPOSER_INSTALL'))
     define('PHPUNIT_COMPOSER_INSTALL', __DIR__ . '/../vendor/autoload.php');
 }
 
+if (!class_exists('Error'))
+{
+    class Error extends Exception
+    {}
+}
+
 date_default_timezone_set('UTC');
