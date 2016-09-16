6b53a7a7fba27410b82c93e92e3a3fafdc6b76ac
Khelle <jamroz.kamil@gmail.com>
2016-09-16 19:11:09 +0200

    KRF-423 #resolve Prepared files for new release

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -14,7 +14,7 @@ class Core extends Container implements CoreInterface
     /**
      * @var string
      */
-    const VERSION = '0.2.2';
+    const VERSION = '0.3.0';
 
     /**
      * @var string
