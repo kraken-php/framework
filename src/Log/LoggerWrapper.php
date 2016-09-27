8b0d0b15cc22e3b67d35243008d296332e50920f
Khelle <jamroz.kamil@gmail.com>
2016-09-27 19:25:15 +0200

    Fix #32 : Fixed logging not working with threads

--- src/Log/LoggerWrapper.php
+++ src/Log/LoggerWrapper.php
@@ -2,7 +2,32 @@
 
 namespace Kraken\Log;
 
+use Monolog\Handler\HandlerInterface as MonologHandlerInterface;
 use Monolog\Logger as Monolog;
 
 class LoggerWrapper extends Monolog
-{}
+{
+    /**
+     * @param string $name The logging channel
+     * @param MonologHandlerInterface[] $handlers Optional stack of handlers, the first one in the array is called first, etc.
+     * @param callable[] $processors Optional array of processors
+     */
+    public function __construct($name, array $handlers = [], array $processors = [])
+    {
+        /**
+         * This is a fix for Pthreads, since that extension does not copy static variables accross threads
+         */
+        static::$levels = [
+            self::DEBUG     => 'DEBUG',
+            self::INFO      => 'INFO',
+            self::NOTICE    => 'NOTICE',
+            self::WARNING   => 'WARNING',
+            self::ERROR     => 'ERROR',
+            self::CRITICAL  => 'CRITICAL',
+            self::ALERT     => 'ALERT',
+            self::EMERGENCY => 'EMERGENCY',
+        ];
+
+        parent::__construct($name, $handlers, $processors);
+    }
+}
